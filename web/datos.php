2d270c9841c4fa5a13800df650557f2a28e01448
josedark <josedark123456789@gmail.com>
2016-05-04 12:39:43 -0600

    Update datos.php

--- web/datos.php
+++ web/datos.php
@@ -9,8 +9,7 @@
     //$result = mysql_query("SELECT * FROM `mediciones`INNER JOIN municipios on mediciones.IdMuni=municipios.IdMuni
       //                     INNER  JOIN departamentos on mediciones.IdDepto=departamentos.IdDepto",$conexion);
    
- $stmt=$conn->prepare("SELECT * FROM mediciones INNER JOIN municipios on mediciones.IdMuni=municipios.IdMuni
-                           INNER  JOIN departamentos on mediciones.IdDepto=departamentos.IdDepto");
+ $stmt=$conn->prepare("SELECT * FROM mediciones ");
  $stmt->execute();
 
 $i=0;
