2af57350b5fa14729f9a44036fc5e8d15ded3e8e
josedark <josedark123456789@gmail.com>
2016-05-04 15:28:59 -0600

    Update insertarMedicion.php

--- web/insertarMedicion.php
+++ web/insertarMedicion.php
@@ -1,77 +1,18 @@
 <?php
 	include("enlacebd.php");
 	
-  // session_start();
+ echo $_POST['FecMedicion'];
+    echo $_POST['ValMedicion'];
+        echo $_POST['selectordepartamentos'];
+        echo $_POST['selectormunicipios'];
+        echo $_POST['UbiMedicion'];
+         echo $_POST['LatMedicion'];
+        echo $_POST['LonMedicion'];
 
 
 	
 	
 
-	if (isset($_POST['FecMedicion'])and isset($_POST['ValMedicion'])and isset($_POST['selectordepartamentos'])and isset($_POST['selectormunicipios'])and isset($_POST['UbiMedicion']) and isset($_POST['LatMedicion'])and isset($_POST['LonMedicion']))
-    {
-		//echo "(NULL, '2015-11-03', '23', '08', '801', 'jose', '23', '34', '1')";
-
-       
-        $FecMediciones =$_POST['FecMedicion'];
-        $ValMediciones =$_POST['ValMedicion'];
-        $IdDepto =$_POST['selectordepartamentos'];
-        $IdMuni =$_POST['selectormunicipios'];
-        $DesUbi =$_POST['UbiMedicion'];
-        $Latitud =$_POST['LatMedicion'];
-        $Longitud =$_POST['LonMedicion'];
-
-   
-        $departamento= $IdDepto [0];
-        $municipio=$IdMuni[0] ;
-
-
-         $stmt=$conn->prepare(' INSERT INTO mediciones (idmediciones, fecmediciones, valMediciones, iddepto, idmuni, desUbi, latitud, longitud, idUsuarios) VALUES (NULL,"'.$FecMediciones.'", '.$ValMediciones.', '.$departamento.', '.$municipio.', '.$DesUbi.', '.$Latitud.', '.$Longitud.', 1)');
-
-          $count=$stmt->execute();
-          
-		if ($count==1) {
-			$_SESSION['r']=" el  proceso  fue exitosamnete .................................";
-			header('Location: /index.php');
-
-			
-		
-
-		} else {
-			$_SESSION['r']= " error" ;
-			header('Location: /index.php');
-			
-			
-		
-		}
-		
-      
-
-
-	}
-	else
-	{
-       
-      //$_SESSION['resultado']="error";
-      //header('Location: /index.php');
-      echo " aqui estoy";
-      try {
-       $stmt=$conn->prepare('INSERT INTO mediciones (idmediciones, fecmediciones, valMediciones, iddepto, idmuni, desUbi, latitud, longitud, idUsuarios) VALUES (NULL, '2015-11-03', '23', '08', '801', 'jose', '23', '34', '1')';
-       $stmt->execute();
-
-    
-
-}catch(PDOException $e){
-
-    echo "ERROR: " . $e->getMessage();
-
-}
-
-      
-      
-     
-		
-		
-	}
 	
 
 ?>
