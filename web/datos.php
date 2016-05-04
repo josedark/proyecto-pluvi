35df05395ffd4e93d3d6e0e369a8c7094bf294bb
josedark <josedark123456789@gmail.com>
2016-05-04 12:51:08 -0600

    Update datos.php

--- web/datos.php
+++ web/datos.php
@@ -13,61 +13,9 @@
  $stmt->execute();
 
 $i=0;
-$respuesta=null;
-
- 
-
- while( $fila = $stmt->fetch() )
- {
-        $IdMediciones =$fila['IdMediciones'];
-        $FecMediciones =$fila['FecMediciones'];
-        $ValMediciones =$fila['ValMediciones'];
-        $IdDepto = "hola";  //$fila['Departamento'];
-        $Municip="hola";    //$fila['nombreMunicipio'];
-        $DesUbi ="hola";//$fila['DesUbi'];
-        $Latitud = "hola";   //$fila['Latitud'];
-        $Longitud = "hola"; //$fila['Longitud'];
-
-        $munici=utf8_encode($Municip);
-        echo ="hola";
-        
-        
-
-        $respuesta->rows[$i]['cell']=array($IdMediciones,$FecMediciones,$ValMediciones,$IdDepto,$munici,$DesUbi,$Latitud ,$Latitud );
-        $i++;
-
-
-
-
- }
-
-   
-   // while ($fila =mysql_fetch_array($result)) {
-     //   $IdMediciones =$fila['IdMediciones'];
-       // $FecMediciones =$fila['FecMediciones'];
-       // $ValMediciones =$fila['ValMediciones'];
-       // $IdDepto =$fila['Departamento'];
-        //$IdMuni =$fila['Municipio'];
-       // $DesUbi =$fila['DesUbi'];
-       // $Latitud =$fila['Latitud'];
-        //$Longitud =$fila['Longitud'];
-       
-        
-        
-
-
-       
-       // $respuesta->rows[$i]['cell']=array($fila['N']=$IdMediciones,$fila["Fecha"]=$FecMediciones,$fila["Medida (ml)"]=$ValMediciones,$fila["Departamento"]=$IdDepto,$fila["Municipio"]=$IdMuni,$fila["Ubicación"]=$DesUbi,$fila["Latitud"]=$Latitud,$fila["Longitud"]=$Longitud);
-       // $i++;
-        
-
-         
-   // }
-
-   
-
+ echo hola;
       
-     echo json_encode($respuesta);
+    
 
 
 
