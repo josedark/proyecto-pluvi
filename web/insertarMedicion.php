1db78de8b7102cd4250cf4a27556169d159cf816
josedark <josedark123456789@gmail.com>
2016-05-04 22:39:17 -0600

    Update insertarMedicion.php

--- web/insertarMedicion.php
+++ web/insertarMedicion.php
@@ -31,9 +31,9 @@
 ini_set('display_startup_errors', 1);
 
         	// phpinfo();
-         //$stmt=$conn->prepare(' INSERT INTO mediciones (fecmediciones, valmediciones, iddepto, idmuni, desubi, latitud, longitud, idusuarios) VALUES ('$FecMediciones','$ValMediciones', '$a','$b','$D','$Latitud', '$Longitud', 1)');
+         $stmt=$conn->prepare(' INSERT INTO mediciones (fecmediciones, valmediciones, iddepto, idmuni, desubi, latitud, longitud, idusuarios) VALUES ('$FecMediciones','$ValMediciones', '$a','$b','$D','$Latitud', '$Longitud', 1)');
 
-       $stmt->execute();
+       
        
       
       
