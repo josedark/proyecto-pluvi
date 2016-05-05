cd2b72b99a199cb1e437202203d48cefaf0abf27
josedark <josedark123456789@gmail.com>
2016-05-04 21:28:37 -0600

    Update insertarMedicion.php

--- web/insertarMedicion.php
+++ web/insertarMedicion.php
@@ -5,23 +5,29 @@
        
        if (isset($_POST['FecMedicion'])and isset($_POST['ValMedicion'])and isset($_POST['selectordepartamentos'])and isset($_POST['selectormunicipios'])and isset($_POST['UbiMedicion']) and isset($_POST['LatMedicion'])and isset($_POST['LonMedicion']))
        {
-       //	 echo $_POST['FecMedicion']."-";
-    //echo $_POST['ValMedicion']."-";
+       	 echo $_POST['FecMedicion']."-";
+    echo $_POST['ValMedicion']."-";
         $a=$_POST['selectordepartamentos']."-";
         $b=$_POST['selectormunicipios']."-";
-      //  echo $_POST['UbiMedicion']."-";
-        // echo $_POST['LatMedicion']."-";
-       // echo $_POST['LonMedicion']."-";
+        echo $_POST['UbiMedicion']."-";
+         echo $_POST['LatMedicion']."-";
+        echo $_POST['LonMedicion']."-";
        echo $a."-";
        echo $b."-";
+       $FecMediciones =$_POST['FecMedicion'];
+        $ValMediciones =$_POST['ValMedicion'];
        
+        $DesUbi =$_POST['UbiMedicion'];
+        $Latitud =$_POST['LatMedicion'];
+        $Longitud =$_POST['LonMedicion'];
         
       
        
         try {
-       //   $stmt=$conn->prepare(' INSERT INTO mediciones (fecmediciones, valmediciones, iddepto, idmuni, desubi, latitud, longitud, idusuarios) VALUES ('.$FecMediciones.', '.$ValMediciones.', '.$a[0].', '.$b[0].','.$D.', '.$Latitud.', '.$Longitud.', 1)');
+         $stmt=$conn->prepare(' INSERT INTO mediciones (fecmediciones, valmediciones, iddepto, idmuni, desubi, latitud, longitud, idusuarios) VALUES ('.$FecMediciones.', '.$ValMediciones.', '.$a.', '.$b.','.$D.', '.$Latitud.', '.$Longitud.', 1)');
 
-         // $count=$stmt->execute();	
+         $count=$stmt->execute();
+         echo  "as";
 
 
 }catch(PDOException $e){
